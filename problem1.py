64f2c26063229b38b1a26ce2ef0f3ec3cbbd5520
vijay <vijay.r091356@gmail.com>
2023-07-05 17:41:44 +0530

    problem1 related to regex solved

---
+++ problem1.py
@@ -0,0 +1,7 @@
+import re
+
+txt = '{"orders":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":648},{"id":649},{"id":650},{"id":651},{"id":652},{"id":653}],"errors":[{"code":3,"message":"[PHP Warning #2] count(): Parameter must be an array or an object that implements Countable (153)"}]}'
+
+result_set = re.findall(r":[\d]+", string=txt)
+result_numbers = list(map(lambda x: int(x[1:]), result_set))
+print(result_numbers)
